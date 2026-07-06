708828a9403098b0a867a962bcdda908a53f3a49
Hamid <abdelhamid.elhouari@outlook.de>
2026-07-06 20:42:34 +0200

    mod file for latest push

--- phoenix-core/src/sql/mod.rs
+++ phoenix-core/src/sql/mod.rs
@@ -1,7 +1,11 @@
+pub mod analyzer;
 pub mod ast;
 pub mod lexer;
+pub mod optimizer;
 pub mod parser;
 
+pub use analyzer::{analyze, type_check_expression, ExprType};
 pub use ast::{DataType, Expression, Statement};
 pub use lexer::{Lexer, LexerError, Token};
+pub use optimizer::{fold_expression, optimize};
 pub use parser::{parse_statement, ParseError, Parser};
